508b302210a3ebfcf8accb71510463682644fabd
Filippo Mazzamuto <filippomazzamuto7@gmail.com>
2021-07-21 20:56:33 +0200

    add section faq + video youtube

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,24 @@
     <main>
       <Section nameClass="container-fluid" component="Services" subTitle="Let’s Dream Big Together" title="We are pioneers of the digital approach, using leading-edge technology to simplify procedures and apply executive coaching in the new age of digitalization."/>
       <Section nameClass="background-lightgrey" component="Carousel" subTitle="Let’s Dream Big Together" title="Explore How can I help you"/>
-      <Section nameClass="" component="GridProduct" subTitle="Let’s Dream Big Together" title="Explore How can I help you"/>
+      <Section component="GridProduct" subTitle="Let’s Dream Big Together" title="Explore How can I help you"/>
+      <!-- inizio sezione faq + video youtube -->
+      <section class="background-violet">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-6 margin-y-10">
+              <iframe width="660" height="415" src="https://www.youtube.com/embed/ElFJ1qcl74U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div class="col-6 margin-y-6 padding-x">
+              <h2 class="title-great color-white">To Thrive In Business Today, You’ll Need a Good Plan</h2>
+              <!-- inizio componente faq -->
+              <Faq/>
+              <!-- fine componente faq -->
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- fine sezione faq + video youtube -->
     </main>
     <footer></footer>
   </div>
@@ -16,13 +33,15 @@ import Header from './components/Header.vue';
 import menu from '@/api/menu.js';
 import Jumbotron from './components/Jumbotron.vue';
 import Section from './components/Section.vue';
+import Faq from './components/Faq.vue';
 
 export default {
   name: 'App',
   components: {
     Header,
     Jumbotron,
-    Section
+    Section,
+    Faq
   },
   data(){
     return{
@@ -41,4 +60,10 @@ export default {
 <style lang="scss">
 @import '@/style/flix.scss';
 @import '@/style/common.scss';
+
+.title-great{
+  font-size: 48px;
+  line-height: 1.17em;
+  margin-bottom: 30px;
+}
 </style>
---
+++ src/components/Faq.vue
@@ -0,0 +1,96 @@
+<template>
+    <div>
+        <div v-for="(faq, index) in itemsFaq" :key="index" :class="countActive === index ? 'active' : null + ' accordion-section'">
+            <div @click="activeAccording(index)" class="according-header">
+                <i :class="countActive === index ? 'far fa-arrow-alt-circle-up' : 'far fa-arrow-alt-circle-down'"></i>
+                <span>{{faq.header}}</span>
+            </div>
+            <div :style="countActive === index ? 'display: block' : null" class="according-content">
+                <p v-for="(content, index) of faq.content" :key="index">{{content}}</p>
+            </div>
+            <hr>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Faq',
+    data(){
+        return {
+            itemsFaq: [
+                {
+                    header: 'How can we help?',
+                    content: [
+                        'How many courses you’ve been on, using however many techniques and methods. Unless you’ve come to realize the value of mindset, then you’ve yet to understand how success comes.',
+                        'With everyone else’s mindset involved impacting too. Hence why business coaching is so crucial for success.'
+                    ]
+                },
+                {
+                    header: 'How can we help?',
+                    content: [
+                        'How many courses you’ve been on, using however many techniques and methods. Unless you’ve come to realize the value of mindset, then you’ve yet to understand how success comes.',
+                        'With everyone else’s mindset involved impacting too. Hence why business coaching is so crucial for success.'
+                    ]
+                },
+                {
+                    header: 'How can we help?',
+                    content: [
+                        'How many courses you’ve been on, using however many techniques and methods. Unless you’ve come to realize the value of mindset, then you’ve yet to understand how success comes.',
+                        'With everyone else’s mindset involved impacting too. Hence why business coaching is so crucial for success.'
+                    ]
+                }
+            ],
+            countActive: 0
+        }
+    },
+    methods: {
+        activeAccording: function(count){
+            if(this.countActive === count){
+                this.countActive = null;
+                return;
+            }
+            this.countActive = count;
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/style/variables.scss';
+
+.according-header{
+    color: #fff;
+    font-size: 1.6rem;
+    cursor: pointer;
+
+    i{
+        margin-right: 20px;
+    }
+
+    &:hover{
+        color: $color-primary;
+    }
+}
+
+
+.according-content{
+    display: none;
+    color: #fff;
+    font-size: 0.8rem;
+    margin-left: 45px;
+
+    & > p{
+        margin: 20px 0;
+    }
+}
+
+.active .according-header{
+    color: $color-primary;
+}
+
+hr{
+    margin: 20px 0;
+}
+
+</style>
